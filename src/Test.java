0835acb5120fd4dca26a75511a99e7caba2dd8f5
bhavishjagani <bhavishjagani2@gmail.com>
2024-05-20 19:43:36 -0700

    Added Main Function

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main (String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Git Commit");
+        System.out.println("Git Push");
     }
 }
 //C:\Users\bjagani\AppData\Local\Programs\Git\mingw64\bin\git.exe
